2dac4f013c4cdec8116e721b48a6589482ff0347
Dev Shah <dshah7971@sdsu.edu>
2023-11-29 15:15:20 -0800

    Final Commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,8 @@ const Footer = () => {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href="/" className="mb-10">
-            {/* <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/> */}
-            DevVentures
+          <Link href="/" className="text-2xl text-green-50">
+            DevVentures 
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,20 +6,24 @@ import Button from "./Button"
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      <Link href="/" className="text-2xl text-green-50">
+        DevVentures
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
             {link.label}
           </Link>
         ))}
       </ul>
 
       <div className="lg:flexCenter hidden">
-        <Button 
+        <Button
           type="button"
           title="Login"
           icon="/user.svg"
@@ -27,7 +31,7 @@ const Navbar = () => {
         />
       </div>
 
-      <Image 
+      <Image
         src="menu.svg"
         alt="menu"
         width={32}
@@ -35,7 +39,7 @@ const Navbar = () => {
         className="inline-block cursor-pointer lg:hidden"
       />
     </nav>
-  )
+  );
 }
 
 export default Navbar
